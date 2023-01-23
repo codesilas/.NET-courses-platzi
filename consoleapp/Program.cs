e994e2b0c031c543984c4b1682c05f0cb7025ca1
codesilas <silas.duarte.h@gmail.com>
2023-01-22 22:14:18 -0600

    ejercicios de compilación y ejecución en .NET

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Humanizer;
+
+Console.WriteLine("Por favor, ingrese un nombre");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Por favor, ingrese su cargo");
+string cargo = Console.ReadLine();
+
+Console.WriteLine("Por favor, ingrese su edad");
+int edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Hola, mi nombre es {nombre}, mi cargo es {cargo}, y tengo {edad.ToWords(new System.Globalization.CultureInfo("es"))} años.");
